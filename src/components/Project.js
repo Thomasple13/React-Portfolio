9f1153ab6d8981fb303184971861a9d430b8deaf
Thomas Le <tle1321@gmail.com>
2022-12-06 21:03:42 -0600

    added project 3

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,7 +13,8 @@ import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const IMAGE1 = "https://imgur.com/RbjDEME.png";
 const IMAGE2 = "https://imgur.com/VthRCKq.png";
-const IMAGE3 = 'https://imgur.com/fGwrT5z.png'
+const IMAGE3 = 'https://imgur.com/fGwrT5z.png';
+const IMAGE4 = 'https://imgur.com/XlM5DZu.png';
 
 export default function Project() {
   return (
@@ -269,6 +270,84 @@ export default function Project() {
                 <Text align={"center"}>
                   The application that you are using right now. A website to
                   showcase my skills and projects.
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Center>
+        <Center m={{ sm: 3, md: 3, lg: 3 }} p={{ sm: 1, md: 2, lg: 2 }}>
+          <Box
+            role={"group"}
+            p={6}
+            maxW={"330px"}
+            w={"full"}
+            bg={useColorModeValue("gray.300", "gray.800")}
+            boxShadow={"2xl"}
+            rounded={"lg"}
+            pos={"relative"}
+            zIndex={1}
+            cursor="pointer"
+          >
+            <Box
+              rounded={"lg"}
+              mt={-12}
+              pos={"relative"}
+              height={"230px"}
+              _after={{
+                transition: "all .3s ease",
+                content: '""',
+                w: "full",
+                h: "full",
+                pos: "absolute",
+                top: 5,
+                left: 0,
+                backgroundImage: `${IMAGE4}`,
+                filter: "blur(15px)",
+                zIndex: -1,
+              }}
+              _groupHover={{
+                _after: {
+                  filter: "blur(40px)",
+                },
+              }}
+            >
+              <Image
+                rounded={"lg"}
+                height={230}
+                width={282}
+                objectFit={"cover"}
+                src={IMAGE4}
+              />
+            </Box>
+            <Stack pt={10} align={"center"}>
+              <Flex>
+                <Button
+                  m={1}
+                  label={"Github"}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    window.open("https://github.com/Lawhornmatt/Jukebox-vite");
+                  }}
+                >
+                  <FaGithub />
+                </Button>
+                <Button
+                  m={1}
+                  label={"Live"}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    window.open("https://jukebox-project3.herokuapp.com/");
+                  }}
+                >
+                  <FaExternalLinkAlt />
+                </Button>
+              </Flex>
+              <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+                Jukebox
+              </Heading>
+              <Stack direction={"row"} align={"center"}>
+                <Text align={"center"}>
+                  An entertaining website that lets people make rooms to share and watch videos together.
                 </Text>
               </Stack>
             </Stack>
